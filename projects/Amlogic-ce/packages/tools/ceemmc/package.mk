48e878e45153141e5c4e11956d0a253d809c885d
longlegmax <longleg+ss@gmail.com>
2023-11-28 14:50:32 +0800

    Update package.mk

--- projects/Amlogic-ce/packages/tools/ceemmc/package.mk
+++ projects/Amlogic-ce/packages/tools/ceemmc/package.mk
@@ -7,19 +7,6 @@ PKG_SITE="https://coreelec.org"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Tool to install CoreELEC on internal eMMC"
 PKG_TOOLCHAIN="manual"
-PKG_VERSION="d8058180fef88a8391f1f3410954922ee60bfa32"
-
-case "${ARCH}" in
-  arm)
-    PKG_SHA256="3b773532f7cb6f2a0934c27da6ee14f57d0fd53da9772846243c708b5cdddcd4"
-    ;;
-  aarch64)
-    PKG_SHA256="5d2826f77eda075d4a24d11dc64111378615aa15baf651638ac638031cebe0ae"
-    ;;
-esac
-
-PKG_SOURCE_NAME="${PKG_NAME}-${ARCH}-${PKG_VERSION}.tar.xz"
-PKG_URL="https://sources.coreelec.org/${PKG_SOURCE_NAME}"
 
 case "${DEVICE}" in
   Amlogic-ng)
@@ -27,8 +14,8 @@ case "${DEVICE}" in
     PKG_SHA256="6c923ac1c022228533b0d19ebb58fc665c3a53a1924c5b5269d6b8795c025c57"
     ;;
   Amlogic-ne)
-    PKG_VERSION="b494d2a5e8f5b83728005b39b87e6f0243236c9f"
-    PKG_SHA256="0804967f3e0c742c4cb9d4f8e90b076663fe9ff0bb647ff273ac33158f532c3f"
+    PKG_VERSION="d8058180fef88a8391f1f3410954922ee60bfa32"
+    PKG_SHA256="5d2826f77eda075d4a24d11dc64111378615aa15baf651638ac638031cebe0ae"
     ;;
 esac
 
